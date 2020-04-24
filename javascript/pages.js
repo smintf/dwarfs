8645d8d1d07ca79c350fac2e2541982eb61388a0
SmintCodes <smintoverflow@gmail.com>
2020-04-24 12:03:07 +0530

    Update pages.js

--- javascript/pages.js
+++ javascript/pages.js
@@ -1,18 +1,3 @@
-function show(shown, hidden) {
-	document.getElementById(shown).style.display = 'block';
-	document.getElementById(hidden).style.display = 'none';
-	return false;
-}
-
-var el = document.getElementById("about");
-if (el) {
-	el.addEventListener("click", function() {
-		alert("Dwarfs: There And Back Again\nGPLv3\nMade with love by mvasilkov and yutyo and smint.");
-	});}
-
-	var el2 = document.getElementById("quit-game");
-	if (el2) {
-		el2.addEventListener("click", function() {
-			window.close();
-		});
-	}
+function show(shown,hidden){document.getElementById(shown).style.display='block';document.getElementById(hidden).style.display='none';return!1}
+var el=document.getElementById("about");if(el){el.addEventListener("click",function(){alert("Dwarfs: There And Back Again\nGPLv3\nMade with love by mvasilkov and yutyo and smint.")})}
+var el2=document.getElementById("quit-game");if(el2){el2.addEventListener("click",function(){window.close()})}
